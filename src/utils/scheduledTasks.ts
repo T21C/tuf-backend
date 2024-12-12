4132c259e7f680a0baff56c2d9435b9e417acb92
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2024-12-12 13:42:48 +0200

    remove unused cache

--- src/utils/scheduledTasks.ts
+++
@@ -1,7 +0,0 @@
-import {updateData} from '../utils/updateHelpers.js';
-
-export const startScheduledTasks = () => {
-  // Update data every 5 minutes
-  const DATA_UPDATE_INTERVAL = 5 * 60 * 1000;
-  setInterval(updateData, DATA_UPDATE_INTERVAL);
-};
