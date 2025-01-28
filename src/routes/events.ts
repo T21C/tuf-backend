27f0ba90a94572dfd1976cde39e770cf5fdcf38d
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-01-28 23:10:11 +0200

    asdgagds

--- src/routes/events.ts
+++ src/routes/events.ts
@@ -5,6 +5,9 @@ const router: Router = Router();
 
 // SSE endpoint
 router.get('/', (req, res) => {
+  console.log('New SSE connection request from:', req.ip);
+  console.log('Headers:', req.headers);
+  
   // Set SSE headers after CORS middleware has run
   res.setHeader('Content-Type', 'text/event-stream');
   res.setHeader('Cache-Control', 'no-cache');
@@ -16,13 +19,16 @@ router.get('/', (req, res) => {
   // Keep the connection alive
   const keepAlive = setInterval(() => {
     res.write(': keepalive\n\n');
+    console.log('Sent keepalive to:', req.ip);
   }, 30000);
 
   // Add client to SSE manager
   const clientId = sseManager.addClient(res);
+  console.log('Added SSE client:', clientId);
 
   // Clean up on connection close
   res.on('close', () => {
+    console.log('SSE connection closed for client:', clientId);
     clearInterval(keepAlive);
     sseManager.removeClient(clientId);
   });
