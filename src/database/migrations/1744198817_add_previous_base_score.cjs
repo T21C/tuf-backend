38d53e87594086368cd5152495388a1ed518030b
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-04-12 18:57:51 +0300

    double for previous basescore

--- src/database/migrations/1744198817_add_previous_base_score.cjs
+++ src/database/migrations/1744198817_add_previous_base_score.cjs
@@ -4,7 +4,7 @@
 module.exports = {
   async up(queryInterface, Sequelize) {
     await queryInterface.addColumn('levels', 'previousBaseScore', {
-      type: Sequelize.INTEGER,
+      type: Sequelize.DOUBLE,
       allowNull: true,
       defaultValue: null
     });
