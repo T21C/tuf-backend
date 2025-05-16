6cfb3b37ef53b4b2934406686af1d1452eccc241
V̠̯̇҉̧͟͢0ͧ̐͒W̎͒ͥ4͆̽ͧNͨͯ͂҉̷͘͝ <vowan.denisov@yandex.ru>
2025-05-16 03:20:24 +0300

    remove unused shell scripts

--- monitor.sh
+++
@@ -1,5 +0,0 @@
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-9.0.0-linux-x86_64.tar.gz
-wget https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-9.0.0-linux-x86_64.tar.gz.sha512
-shasum -a 512 -c elasticsearch-9.0.0-linux-x86_64.tar.gz.sha512
-tar -xzf elasticsearch-9.0.0-linux-x86_64.tar.gz --strip-components=1 -C elasticsearch-9.0.0
-cd elasticsearch-9.0.0/
